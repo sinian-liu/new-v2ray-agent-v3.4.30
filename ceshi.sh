72860f3f9fa55dcda3c0bfc9bb15fb524f5311db
sinian-liu <86273555+sinian-liu@users.noreply.github.com>
2025-02-17 17:38:14 +0800

    Update ceshi.sh

--- ceshi.sh
+++ ceshi.sh
@@ -746,8 +746,3 @@ EOF
                     sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
                     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
                     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-                    sudo apt-get update
-                    sudo apt-get install -y docker-ce
-                elif [ "$SYSTEM" == "centos" ]; then
-                    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-                    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
